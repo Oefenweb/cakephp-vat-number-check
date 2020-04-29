21b685ad0705c00b18f537358de52547d3570df9
Michael Ehrlein <ehrlein@stahlnetz.de>
2020-04-29 11:43:49 +0200

    Revert "disabled deprecation warnings in Tests"
    This reverts commit 8eb5a89e

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -83,10 +83,6 @@ Cache::config([
     ],
 ]);
 
-Configure::write('Error', [
-    'errorLevel' => E_ALL & ~E_USER_DEPRECATED,
-]);
-
 if (!getenv('db_dsn')) {
     putenv('db_dsn=sqlite:///:memory:');
 }

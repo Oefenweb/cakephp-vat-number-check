9e87b756601b506865c006a82a56154da80d539a
Mischa ter Smitten <mischa@tersmitten.nl>
2020-04-30 22:16:44 +0200

    Consistency changes

--- .travis/before-script.sh
+++ .travis/before-script.sh
@@ -17,6 +17,11 @@ elif [ "${PHP_CPD}" = '1' ]; then
     exit 0;
 elif [ "${PHP_MD}" = '1' ]; then
     exit 0;
+elif [ "${PHP_STAN}" = '1' ]; then
+    exit 0;
+elif [ "${PHP_PHAN}" = '1' ]; then
+    pecl install ast;
+    exit 0;
 fi
 
 composer require "cakephp/cakephp:${CAKE_VERSION}"  --dev --no-ansi --no-progress --no-interaction;
